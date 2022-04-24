e61096695d52dc14e1b598ce9105842a08bcbf45
victor <victor.eduardo.sntos@gmail.com>
2022-04-24 16:14:34 -0300

    editar usuario melhorado

---
+++ src/app/shared/service/endereco.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+import {take} from "rxjs/operators";
+import {Endereco} from "../model/endereco.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnderecoService {
+
+  apiUrl = `${environment.API}enderecos`;
+  httpOptions = {
+    heeaders: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
+
+  public getUsuarioById(id: number): Observable<Endereco> {
+    return this.httpClient.get<Endereco>(`${this.apiUrl}/${id}`).pipe(take(1))
+  }
+
+  public create(endereco: Endereco) {
+    return this.httpClient.post(this.apiUrl, endereco).pipe(take(1));
+  }
+
+  public update(endereco: Endereco) {
+    return this.httpClient.put(`${this.apiUrl}/${endereco.id}`, endereco).pipe(take(1));
+  }
+}
